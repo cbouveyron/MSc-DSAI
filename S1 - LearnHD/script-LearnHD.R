d3cbf7a5a2909f05105cdfe88865e85cb812f763
Charles Bouveyron <charles.bouveyron@math.cnrs.fr>
2019-10-31 11:26:43 +0100

    Update of Oct. 31

--- S1 - LearnHD/script-LearnHD.R
+++ S1 - LearnHD/script-LearnHD.R
@@ -24,6 +24,38 @@ biplot(pc)
 
 # Exercice: Implement the use of BIC for selecting d in PPCA
 ????
+  
+# Exercice: apply t-SNE on the Decathlon data and compare
 
-# Day 3: Coding a basic version of t-SNE
+# Day 3: GMM for Classification
 ##########################################################
+
+library(MASS)
+data(iris)
+plot(iris[,-5],col=iris$Species)
+
+
+Y = iris$Species
+X = iris[,-5]
+
+learn = sample(1:150,100)
+qda.clf = qda(X[learn,],Y[learn])
+
+plot(iris[learn,1:2],col=Y[learn])
+points(qda.clf$means,col=1:3,pch=19,cex=3)
+
+pred = predict(qda.clf,X[-learn,])
+points(X[128,],col=4,pch=17,cex=5)
+sum(pred$class != Y[-learn])
+
+lda.clf = lda(X[learn,],Y[learn])
+pred = predict(lda.clf,X[-learn,])
+sum(pred$class != Y[-learn])
+
+
+
+
+
+
+
+
